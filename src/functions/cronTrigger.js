56df43d4765c9d77b9729d98ef69d011e815c3ec
Neven Kajić <nkajic@users.noreply.github.com>
2023-11-25 14:17:48 +0100

    Update cronTrigger.js

--- src/functions/cronTrigger.js
+++ src/functions/cronTrigger.js
@@ -132,11 +132,11 @@ export async function processCronTrigger(event) {
         checkLocation
       ].n
 
-      const min = monitorsState.monitors[monitor.id].checks[checkDay].res[checkLocation].min || 0
+      const min = monitorsState.monitors[monitor.id].checks[checkDay].res[checkLocation].min || 999999
       if (requestTime < min) {
         min = requestTime
       }
-      monitorsState.monitors[monitor.id].checks[checkDay].res[checkLocation].min = min || 999999
+      monitorsState.monitors[monitor.id].checks[checkDay].res[checkLocation].min = min
       
       const max = monitorsState.monitors[monitor.id].checks[checkDay].res[checkLocation].max || 0
       if (requestTime > max) {
