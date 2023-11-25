10f12bd07bed57e4c6d2ed4d24d5bf03fa8a3fb8
Neven Kajić <nkajic@users.noreply.github.com>
2023-11-25 17:21:46 +0100

    Update monitorResponses.js

--- src/components/monitorResponses.js
+++ src/components/monitorResponses.js
@@ -2,9 +2,16 @@ import React from 'react'
 import config from '../../config.yaml'
 
 export default function MonitorResponses({ monitorId, kvMonitor }) {
+
+  let date = new Date()
+  const dayInHistogram = date.toISOString().split('T')[0]
+  let content = dayInHistogram
+
+  
+  
   return (
     <div className="text-gray-400 text-sm">
-      Neven was here
+      {content}
     </div>
   )
 }
