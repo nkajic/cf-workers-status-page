79cf4a4f93610cc292ac46c3d80c1ab169ceb078
Neven Kajić <nkajic@users.noreply.github.com>
2023-11-25 17:12:31 +0100

    Create monitorResponses.js

---
+++ src/components/monitorResponses.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import config from '../../config.yaml'
+
+export default function MonitorResponses({ monitorId, kvMonitor }) {
+  return (
+    <div>
+      Neven was here
+    </div>
+  )
+}
