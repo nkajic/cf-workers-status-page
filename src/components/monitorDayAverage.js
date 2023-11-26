7eee3d87a1b6950eb16e1dc81857d79a024e1465
Neven Kajić <nkajic@users.noreply.github.com>
2023-11-26 10:06:32 +0100

    Update monitorDayAverage.js

--- src/components/monitorDayAverage.js
+++ src/components/monitorDayAverage.js
@@ -1,11 +1,16 @@
 import { locations } from '../functions/locations'
 
-export default function MonitorDayAverage({ location, min, max, avg }) {
+export default function MonitorDayAverage({ location, min, max, avg, n }) {
+
+  min = min / 1000
+  max = max / 1000
+  avg = avg / 1000
+  
   return (
     <>
       <br />
       <small>
-        {locations[location] || location}: {min} / {max} / {avg} ms
+        {locations[location] || location}: {min} / {max} / {avg} sec (n={n})
       </small>
     </>
   )
