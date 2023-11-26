5c8703c06f3edaad164d16aa96554d783ac53e44
Neven Kajić <nkajic@users.noreply.github.com>
2023-11-26 15:13:54 +0100

    Update monitorResponses.js

--- src/components/monitorResponses.js
+++ src/components/monitorResponses.js
@@ -3,14 +3,21 @@ import config from '../../config.yaml'
 
 export default function MonitorResponses({ monitorId, kvMonitor }) {
 
-  // let date = new Date()
-  // const dayInHistogram = date.toISOString().split('T')[0]
-  // let content = dayInHistogram
+  const LOC = 'ZAG'
+  
+  let date = new Date()
+  const dayInHistogram = date.toISOString().split('T')[0]
+  const location = kvMonitor.checks[dayInHistogram].res[LOC] || {}
+  const reqs = location.r || {}
+
+  let content = JSON.stringify(reqs)
 
   
   
   return (
     <div className="text-gray-400 text-sm">
+    Zagreb times: <br />
+    {content}
     </div>
   )
 }
