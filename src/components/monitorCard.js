4c469dfca73338b471a7bb533238f0c94296508d
Neven Kajić <nkajic@users.noreply.github.com>
2023-11-26 17:04:34 +0100

    Update monitorCard.js

--- src/components/monitorCard.js
+++ src/components/monitorCard.js
@@ -27,7 +27,7 @@ export default function MonitorCard({ key, monitor, data }) {
   
   return (
     <div key={key} className={isCardOpen ? 'card open' : 'card'}>
-      <button onClick={toggleCardOpen}>Otpri</button>
+      <button onClick={toggleCardOpen}>Otpri {isCardOpen}</button>
       <div className="flex flex-row justify-between items-center mb-2">
         <div className="flex flex-row items-center align-center">
           {monitor.description && (
