9813e68b2bc22d3dd13126c27ac0280f79c3c383
Neven Kajić <nkajic@users.noreply.github.com>
2023-11-26 09:43:47 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,9 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
     <div className="min-h-screen">
       <Head>
         <title>{config.settings.title}</title>
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="theme-color" content="#fff">
         <link rel="stylesheet" href="./style.css" />
         <script>
           {`
