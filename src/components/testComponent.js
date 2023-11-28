7745fc370a206c26390794f6c32b1e49dad3b184
Neven Kajić <nkajic@users.noreply.github.com>
2023-11-28 21:34:30 +0100

    Create testComponent.js

---
+++ src/components/testComponent.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react'
+
+export default function TestComponent() {
+  let someFlag = 'Keks1'
+  return (
+    <div>
+      1
+      someFlag: {someFlag}
+    </div>
+  )
+}
